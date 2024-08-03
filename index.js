463246b4d33636459ec83090fa142005ea7ba868
Godfrey Onyango <godiodhis268@gmail.com>
2024-08-03 12:54:44 +0300

    last

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+let employee={
+    name:"Godi",
+    streetAdress:"Tom Mboya",
+}
+function  updateEmployeeWithKeyAndValue(employee, key, value){
+ let newEmployee= {...employee}
+    newEmployee[key]=value;
+    console.log(newEmployee)
+    return newEmployee;
+ 
+}
+updateEmployeeWithKeyAndValue(employee,"name","Jimmy");
+function  destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key]=value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key){
+    let clone={...employee}
+    delete clone[key];
+    return clone;
+    
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
